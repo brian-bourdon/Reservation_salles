b541d2a013d3633cbc0c61510b5590de61efe2a5
Unknown <Kyrigami@gmail.com>
2019-05-09 16:52:19 +0100

    Ajout de la table Salle, et mise a jour de la table rendez

--- database_scripts/projetannuel.sql
+++ database_scripts/projetannuel.sql
@@ -1,22 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
--- https://www.phpmyadmin.net/
+-- version 4.1.14
+-- http://www.phpmyadmin.net
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 09 mai 2019 à 13:53
--- Version du serveur :  5.7.24
--- Version de PHP :  7.2.14
+-- Client :  127.0.0.1
+-- Généré le :  Jeu 09 Mai 2019 à 17:50
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données :  `projetannuel`
@@ -28,7 +26,6 @@ SET time_zone = "+00:00";
 -- Structure de la table `rendez_vous`
 --
 
-DROP TABLE IF EXISTS `rendez_vous`;
 CREATE TABLE IF NOT EXISTS `rendez_vous` (
   `idRdv` int(11) NOT NULL AUTO_INCREMENT,
   `idDemandeur` int(11) NOT NULL,
@@ -36,15 +33,30 @@ CREATE TABLE IF NOT EXISTS `rendez_vous` (
   `HeureDebut` time NOT NULL,
   `HeureFin` time NOT NULL,
   `Salle` varchar(50) NOT NULL,
+  `status` varchar(255) NOT NULL,
   PRIMARY KEY (`idRdv`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Déchargement des données de la table `rendez_vous`
+-- Contenu de la table `rendez_vous`
 --
 
-INSERT INTO `rendez_vous` (`idRdv`, `idDemandeur`, `idInterlocuteur`, `HeureDebut`, `HeureFin`, `Salle`) VALUES
-(1, 9, 9999, '15:00:00', '16:00:00', 'A13');
+INSERT INTO `rendez_vous` (`idRdv`, `idDemandeur`, `idInterlocuteur`, `HeureDebut`, `HeureFin`, `Salle`, `status`) VALUES
+(1, 9, 9999, '15:00:00', '16:00:00', 'A13', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `salle`
+--
+
+CREATE TABLE IF NOT EXISTS `salle` (
+  `idSalle` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) NOT NULL,
+  `capacite` int(11) NOT NULL,
+  `statut` varchar(255) NOT NULL,
+  PRIMARY KEY (`idSalle`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -52,7 +64,6 @@ INSERT INTO `rendez_vous` (`idRdv`, `idDemandeur`, `idInterlocuteur`, `HeureDebu
 -- Structure de la table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `idUser` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
@@ -61,10 +72,10 @@ CREATE TABLE IF NOT EXISTS `user` (
   `statut` varchar(255) NOT NULL,
   `pwd` varchar(255) NOT NULL,
   PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
--- Déchargement des données de la table `user`
+-- Contenu de la table `user`
 --
 
 INSERT INTO `user` (`idUser`, `email`, `nom`, `prenom`, `statut`, `pwd`) VALUES
@@ -78,7 +89,6 @@ INSERT INTO `user` (`idUser`, `email`, `nom`, `prenom`, `statut`, `pwd`) VALUES
 (11, 'bbourdon1@myges.frfff', 'rgzbff', 've', 'professeur', '098f6bcd4621d373cade4e832627b4f6'),
 (15, 'fbve@fze.com', 'gvbeyqv', 'fbz', 'etudiant', '098f6bcd4621d373cade4e832627b4f6'),
 (16, 'fbve@fze.com', 'gvbeyqv', 'fbz', 'professeur', '098f6bcd4621d373cade4e832627b4f6');
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
