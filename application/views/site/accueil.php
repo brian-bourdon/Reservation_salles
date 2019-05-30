0961456768c0b7a3b6a53a0c81891a42bf5ff41d
Unknown <Kyrigami@gmail.com>
2019-05-30 19:02:19 +0200

    23:59 limite

--- application/views/site/accueil.php
+++ application/views/site/accueil.php
@@ -211,7 +211,7 @@
                                 <input class="form-control" type="text" id="titresalle" name="salle" value="" placeholder="Salle" required="" readonly/><br>  
                                 <input class="form-control" type="text" name="date" id="datesalle" value="" placeholder="date" required="" readonly/><br>
                                 <input class="form-control" type="time" name="heure_debut" id="heure_debut1" value="" placeholder="Heure de début" required="" readonly/><br>
-                                <input class="form-control" type="time"  min="" max="23:00" name="heure_fin" id="heure_fin1" value="" required=""/><br>
+                                <input class="form-control" type="time"  min="" max="23:59" name="heure_fin" id="heure_fin1" value="" required=""/><br>
                                 <input class="form-control searchUser" type="text" id="nom_prof" name="nom_prof" value="" placeholder="Nom du professeur et/ou nom des membres de votre groupe" required=""/><br>
                                 <input class="form-control searchUser" type="hidden" id="idProf" name="idProf" value=""/>
                                 <div id="result"></div>
