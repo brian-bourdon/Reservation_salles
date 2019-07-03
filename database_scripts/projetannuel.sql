4f1e0b0b205dc1c6bbfb54dde8500f2e702ba708
brian <brian78.b@gmail.com>
2019-07-03 18:04:08 +0200

    Update BDD

--- database_scripts/projetannuel.sql
+++ database_scripts/projetannuel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 29 mai 2019 à 14:06
+-- Généré le :  mer. 03 juil. 2019 à 16:03
 -- Version du serveur :  5.7.24
 -- Version de PHP :  7.2.14
 
@@ -25,6 +25,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `notification`
+--
+
+DROP TABLE IF EXISTS `notification`;
+CREATE TABLE IF NOT EXISTS `notification` (
+  `idNotif` int(11) NOT NULL AUTO_INCREMENT,
+  `idRdv` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  PRIMARY KEY (`idNotif`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `notification`
+--
+
+INSERT INTO `notification` (`idNotif`, `idRdv`, `idUser`) VALUES
+(11, 150, 19);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `rendez_vous`
 --
 
@@ -37,22 +58,19 @@ CREATE TABLE IF NOT EXISTS `rendez_vous` (
   `HeureDebut` time NOT NULL,
   `HeureFin` time NOT NULL,
   `idSalle` varchar(50) NOT NULL,
-  `status` varchar(255) NOT NULL,
+  `statut` enum('accepted','refused','waiting','') NOT NULL DEFAULT 'waiting',
   `titre` varchar(10) NOT NULL,
   PRIMARY KEY (`idRdv`)
-) ENGINE=InnoDB AUTO_INCREMENT=128 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=154 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `rendez_vous`
 --
 
-INSERT INTO `rendez_vous` (`idRdv`, `idDemandeur`, `idInterlocuteur`, `Date`, `HeureDebut`, `HeureFin`, `idSalle`, `status`, `titre`) VALUES
-(1, 9, 9999, '2019-05-29', '15:00:00', '16:00:00', '1', '', 'A01'),
-(2, 9, 0, '2019-05-29', '12:00:00', '14:00:00', '1', '', 'A01'),
-(3, 9, 0, '2019-05-29', '13:00:00', '14:00:00', '2', '', 'A02'),
-(4, 1, 9, '2019-05-29', '15:00:00', '16:00:00', '2', '', 'A02'),
-(6, 15, 9, '2019-05-29', '17:00:00', '19:00:00', '1', '', 'A01'),
-(126, 15, 9, '2019-05-29', '15:00:00', '23:59:00', '3', '', 'A03');
+INSERT INTO `rendez_vous` (`idRdv`, `idDemandeur`, `idInterlocuteur`, `Date`, `HeureDebut`, `HeureFin`, `idSalle`, `statut`, `titre`) VALUES
+(151, 18, 19, '2019-07-03', '18:00:00', '19:00:00', '1', 'accepted', 'A01'),
+(152, 18, 20, '2019-07-03', '18:00:00', '19:00:00', '1', 'accepted', 'A01'),
+(153, 18, 20, '2019-07-03', '18:00:00', '19:00:00', '2', 'refused', 'A02');
 
 -- --------------------------------------------------------
 
@@ -93,23 +111,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `statut` varchar(255) NOT NULL,
   `pwd` varchar(255) NOT NULL,
   PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`idUser`, `email`, `nom`, `prenom`, `statut`, `pwd`) VALUES
-(1, 'k@gmail.fr', 'DOSSOU', 'KYRIEL', 'professeur', 'f23ccd066f8236c6f97a2a62d3f9f9f5'),
-(5, 'kdossou1@myges.fr', 'DOSSOU', 'Kyriel', 'etudiant', 'df8fede7ff71608e24a5576326e41c75'),
-(6, 'dossou1@myges.com', 'DOSSOU', 'Kyriel', 'etudiant', 'ceb8447cc4ab78d2ec34cd9f11e4bed2'),
-(7, 'McDo@food.fr', 'Mc', 'Donalds', 'etudiant', '62506be34d574da4a0d158a67253ea99'),
-(8, 'boobs@hot.fr', 'logic', 'Boobs', 'etudiant', 'd091fccc62e2d24ab101dbe01ce844f6'),
-(9, 'zdsf@myges.com', 'Brian', 'Bourdon', 'etudiant', '098f6bcd4621d373cade4e832627b4f6'),
-(10, 'fzuegfzu@myges.comfvv', 'rgzb', 'vqf', 'etudiant', '098f6bcd4621d373cade4e832627b4f6'),
-(11, 'bbourdon1@myges.frfff', 'rgzbff', 've', 'professeur', '098f6bcd4621d373cade4e832627b4f6'),
-(15, 'fbve@fze.com', 'gvbeyqv', 'fbz', 'etudiant', '098f6bcd4621d373cade4e832627b4f6'),
-(17, 'test1@test.com', 'Hugo', 'FRY', 'etudiant', '5fa8fd8b170efb73d331d89c549f80dd');
+(18, 'brian78.b@gmail.com', 'Bourdon', 'Brian', 'etudiant', '098f6bcd4621d373cade4e832627b4f6'),
+(19, 'kyriel@gmail.com', 'test', 'Kyriel', 'etudiant', '098f6bcd4621d373cade4e832627b4f6'),
+(20, 'kevin@gmail.com', 'test', 'Kevin', 'etudiant', '098f6bcd4621d373cade4e832627b4f6');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
